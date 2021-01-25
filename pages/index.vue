b4b247873c4781a4311df83bc57400e1995fa051
Ryota Niinomi Takemoto <r21nomi@gmail.com>
2021-01-25 09:51:39 +0900

    Add initial code

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="topPage">
+    <h1>Hello, PCD Tokyo!!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component({
+  components: {},
+})
+export default class TopPage extends Vue {}
+</script>
+
+<style scoped lang="stylus">
+.topPage
+  h1
+    color red
+    text-align center
+</style>
