504f053f65feffc27c2fe6e6d961bbd2f6a9250c
Ryota Niinomi Takemoto <r21nomi@gmail.com>
2021-02-06 23:15:40 +0900

    Modify text

--- pages/submit.vue
+++ pages/submit.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="submitPage">
     <Header />
-    <div class="container">How to submit your work to this gallery.</div>
+    <div class="container">
+      <h2 class="title">あなたのスケッチ・作品でPCD Japanを盛り上げよう</h2>
+      <p class="description">
+        ページ下部のボタンから誰でもスケッチ・作品を本ギャラリーに投稿できます。<br />オンラインで閲覧できるスケッチ・作品であれば何でも可能です。<br />OpenProcessingやp5.js
+        web editor、自分のWebサイトなど、あなたのスケッチ・作品を共有してPCD
+        Japanを盛り上げましょう！
+      </p>
+    </div>
     <ArtSubmissionButton />
   </div>
 </template>
@@ -22,8 +29,17 @@ export default class SubmitPage extends Vue {}
 
 .submitPage
   .container
+    color $white_fff
     text-align center
     margin-top $margin_48
+
+    .title
+      font-size $font_size_36
+
+    .description
+      font-size $font_size_14
+      line-height 2.4rem
+      margin-top $margin_20
 
   .artSubmissionButton
     margin-top $margin_80
