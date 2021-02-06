ef94f94037d352dc36a62390facf123b826a50e6
Ryota Niinomi Takemoto <r21nomi@gmail.com>
2021-02-06 22:56:49 +0900

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <div class="app">
     <Nuxt />
+    <Footer />
   </div>
 </template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+import Footer from '@/components/Footer'
+@Component({
+  components: { Footer },
+})
+export default class extends Vue {}
+</script>
+
+<style scoped lang="stylus">
+@require '~@/assets/style/variables'
+@require '~@/assets/style/mixin'
+
+.app
+  .footer
+    margin-top $margin_20
+</style>
